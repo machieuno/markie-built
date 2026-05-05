fb1dfb6eadb3a9b9da943564f049eace8fe635f2
machieuno <rolackmarquel@gmail.com>
2026-05-05 14:06:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ setTimeout(() => {
 intro.style.display = "none";
 }, 3000);
 });
+
+/* smooth scroll */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener("click", function(e) {
+e.preventDefault();
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({ behavior: "smooth" });
+});
+});
